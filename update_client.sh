c0c0e18e1f538d2a714463a1058314d7e5a38920
Richard Mahn <richmahn@users.noreply.github.com>
2022-02-16 07:12:33 -0700

    Update for v1.16.2

--- update_client.sh
+++ update_client.sh
@@ -3,9 +3,9 @@
 set -x
 set -e
 
-version=$(curl -H 'Content-type: application/json' https://git.door43.org/api/v1/version | jq -r '.version'| cut -f1 -d"+")
+version=$(curl -H 'Content-type: application/json' https://qa.door43.org/api/v1/version | jq -r '.version'| cut -f1 -d"+")
 
-curl -H 'Content-type: application/json' -X POST -d "{\"options\": {\"npmName\": \"dcs_api_client\", \"npmVersion\": \"$version\", \"supportsES6\": \"true\"}, \"swaggerUrl\": \"https://git.door43.org/swagger.v1.json\"}" https://generator.swagger.io/api/gen/clients/typescript-node | curl $(jq -r '.link') -o client.zip
+curl -H 'Content-type: application/json' -X POST -d "{\"options\": {\"npmName\": \"dcs_api_client\", \"npmVersion\": \"$version\", \"supportsES6\": \"true\"}, \"swaggerUrl\": \"https://qa.door43.org/swagger.v1.json\"}" https://generator.swagger.io/api/gen/clients/typescript-node | curl $(jq -r '.link') -o client.zip
 
 unzip client.zip
 
